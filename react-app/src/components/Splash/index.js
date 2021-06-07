720b336c757d27534e9df76b3d4ad8317a8f9b48
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2021-06-07 13:06:33 -0500

    splash started

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,151 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './splash.css';
+import chad from '../../context/chadback1.png';
 
 export default function Splash () {
 
 
 
     return (
-        <>
-        <div class="container">
-
-	<div class="rain">
-		<div class="drop"></div>
-		<div class="waves">
-			<div></div>
-			<div></div>
-		</div>
-		<div class="splash"></div>
-		<div class="particles">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	</div>
-
-	<div class="rain">
-		<div class="drop"></div>
-		<div class="waves">
-			<div></div>
-			<div></div>
-		</div>
-		<div class="splash"></div>
-		<div class="particles">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	</div>
-
-	<div class="rain">
-		<div class="drop"></div>
-		<div class="waves">
-			<div></div>
-			<div></div>
-		</div>
-		<div class="splash"></div>
-		<div class="particles">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	</div>
-
-	<div class="rain">
-		<div class="drop"></div>
-		<div class="waves">
-			<div></div>
-			<div></div>
-		</div>
-		<div class="splash"></div>
-		<div class="particles">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	</div>
-
-	<div class="rain">
-		<div class="drop"></div>
-		<div class="waves">
-			<div></div>
-			<div></div>
-		</div>
-		<div class="splash"></div>
-		<div class="particles">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	</div>
-
-	<div class="rain">
-		<div class="drop"></div>
-		<div class="waves">
-			<div></div>
-			<div></div>
-		</div>
-		<div class="splash"></div>
-		<div class="particles">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	</div>
-
-	<div class="rain">
-		<div class="drop"></div>
-		<div class="waves">
-			<div></div>
-			<div></div>
-		</div>
-		<div class="splash"></div>
-		<div class="particles">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	</div>
-
-	<div class="rain">
-		<div class="drop"></div>
-		<div class="waves">
-			<div></div>
-			<div></div>
-		</div>
-		<div class="splash"></div>
-		<div class="particles">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	</div>
-
-	<div class="rain">
-		<div class="drop"></div>
-		<div class="waves">
-			<div></div>
-			<div></div>
-		</div>
-		<div class="splash"></div>
-		<div class="particles">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	</div>
-
+        <div class="main">
+<div class="rainy-weather">
+<div class="cloud-main"></div>
+<div class="cloud-center"></div>
+<div class="cloud-left"></div>
+<div class="droplet droplet1"></div>
+<div class="droplet droplet2"></div>
+<div class="droplet droplet3"></div>
+<div class="droplet droplet4"></div>
+<div class="droplet droplet5"></div>
+<div class="droplet droplet6"></div>
 </div>
-        </>
+<img class="chad" src={chad} ></img> //make chad shake 
+        </div>
     )
 }
