13ab4290e8b1af225c71a79bba7a220c46635661
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2021-06-07 19:52:39 -0500

    splash finished

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -2,25 +2,34 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './splash.css';
 import chad from '../../context/chadback1.png';
+import banner from '../Homepage/banner.png';
+
 
 export default function Splash () {
 
 
 
     return (
-        <div class="main">
-<div class="rainy-weather">
-<div class="cloud-main"></div>
-<div class="cloud-center"></div>
-<div class="cloud-left"></div>
-<div class="droplet droplet1"></div>
-<div class="droplet droplet2"></div>
-<div class="droplet droplet3"></div>
-<div class="droplet droplet4"></div>
-<div class="droplet droplet5"></div>
-<div class="droplet droplet6"></div>
-</div>
-<img class="chad" src={chad} ></img> //make chad shake 
+        <>
+        <div className='outer'>
         </div>
+        <div className="main">
+            <img src={banner}></img>
+<div className="rainy-weather">
+<div className="cloud-main"></div>
+<div className="cloud-center"></div>
+<div className="cloud-left"></div>
+<div className="droplet droplet1"></div>
+<div className="droplet droplet2"></div>
+<div className="droplet droplet3"></div>
+<div className="droplet droplet4"></div>
+<div className="droplet droplet5"></div>
+<div className="droplet droplet6"></div>
+
+</div>
+<img class="chad" src={chad} ></img> 
+<h2 className="welcome">Welcome to Depressed Bro!  An app where you can post your woes and help your bros! Click <NavLink className='tag' to='/home'>here</NavLink> to get started.</h2>
+        </div>
+        </>
     )
 }
